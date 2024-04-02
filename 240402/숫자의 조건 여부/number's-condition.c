5d634a7c25eb082ba2f6e85b7319a1338500e2c1
rlarkdms1003 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-02 23:42:15 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/숫자의 조건 여부/number's-condition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if (a>=113) {
+        printf("1", a);
+    }
+    else {
+        printf("0", a);
+    }
+    return 0;
+}
