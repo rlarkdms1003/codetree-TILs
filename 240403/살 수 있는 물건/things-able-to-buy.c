2ea7b97d779dbbcf536ac0ef58af2dd80e9bfd1f
rlarkdms1003 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-03 15:49:30 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240403/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n>= 3000) {
+        printf("book");
+    }
+    else if (n>=1000) {
+        printf("mask");
+    }
+    else {
+        printf("no");
+    }
+    return 0;
+}
