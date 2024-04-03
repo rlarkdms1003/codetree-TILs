334a69e58ae7f71f36abf8ac382d15562033ced1
rlarkdms1003 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-03 14:59:12 +0900

    Title: 삼항연산자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/삼항연산자/ternary-operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int score;
+    scanf("%d", &score);
+    if (score = 100) {
+        printf("pass");
+    }
+    else {
+        printf("failure");
+    }
+    return 0;
+}
