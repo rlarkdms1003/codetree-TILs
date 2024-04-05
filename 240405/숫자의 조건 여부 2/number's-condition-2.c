70ef2ce986287ea93647e0a29b8e71c78032e407
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-05 15:06:19 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240405/숫자의 조건 여부 2/number's-condition-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    if (a==5) {
+        printf("A");
+        }
+    if (a/2==0) {
+        printf("B");
+    }
+    else {
+        printf("");
+    }
+    return 0;
+}
