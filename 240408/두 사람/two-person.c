e041e5b0d12e66037a100f35aaa5219ec1f4b103
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-08 16:27:00 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/두 사람/two-person.c
+++ 240408/두 사람/two-person.c
@@ -5,10 +5,7 @@ int main() {
     char a_sex, b_sex;
     scanf("%d %c", &a_age, &a_sex);
     scanf("%d %c", &b_age, &b_sex);
-    if(a_age && a_sex >=19 || b_age && b_sex >=19) {
-        printf("1");
-    }
-    else if ((a_age && a_sex >= 19) && (b_age && b_sex >=19)){
+    if((a_age>=19 && a_sex== 'M') || (b_age >=19 && b_sex =='M')) {
         printf("1");
     }
     else {
