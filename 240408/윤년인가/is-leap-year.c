89c0ba2ecf80cfaf0d0b2310658ce84a81d2ac50
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-08 17:01:16 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240408/윤년인가/is-leap-year.c
+++ 240408/윤년인가/is-leap-year.c
@@ -3,13 +3,14 @@
 int main() {
     int y;
     scanf("%d", &y);
-    if (y/4!=0) {
-        if (y/100==0 && y/400!=0) {
-            printf("false");
-        }
-        else {
-            printf("true");
-        }
+    if (y%4==0) {
+        printf("true");
+    }
+    else if (y%100==0 && y%400!=0) {
+        printf("false");
+    }
+    else {
+        printf("false");
     }
     return 0;
 }
