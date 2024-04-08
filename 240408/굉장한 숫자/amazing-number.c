8636c27eb536f95aedc873eb5077153c485240bc
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-08 12:06:18 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/굉장한 숫자/amazing-number.c
+++ 240408/굉장한 숫자/amazing-number.c
@@ -6,7 +6,9 @@ int main() {
     if ((n%2==1 && n%3==0) || (n%2==0 && n%5==0)) {
         printf("true");
     }
-    else ("false");
+    else {
+        printf("false");
+    }
     
     return 0;
 }
