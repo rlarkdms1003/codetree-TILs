aa0dbec705750d2ad805d4722177d18eafd48213
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-04-11 17:10:34 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240411/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a_temp, a_cold, b_temp, b_cold, c_temp, c_cold;
+    scanf("%d %d", &a_cold, &a_temp);
+    scanf("%d %d", &b_cold, &b_temp);
+    scanf("%d %d", &c_cold, &c_temp);
+
+    if((a_cold=='Y' && a_temp>=37) && (b_cold=='Y' && b_temp>=37) &&(c_cold=='Y' && c_temp>=37)) {
+        printf("A");
+    }
+    else if((a_cold=='Y' && a_temp>=37) && (b_cold=='Y' && b_temp>=37 || c_cold=='Y' && c_temp>=37)) {
+        printf("A");
+    }
+    else if()
+    return 0;
+}
