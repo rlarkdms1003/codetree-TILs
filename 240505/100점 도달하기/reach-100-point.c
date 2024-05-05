9c6b16921d4cdd8b8acf71f04c68dfa024b6b405
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-05 16:35:52 +0900

    Title: 100점 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240505/100점 도달하기/reach-100-point.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    for(i=n; i<=100; i++) {
+        if(i>=90) {
+            printf("A ");
+        }
+        else if(i>=80) {
+            printf("B ");
+        }
+        else if(i>=70) {
+            printf("C ");
+        }
+        else if(i>=60) {
+            printf("D ");
+        }
+        else {
+            printf("F ");
+        }
+    }
+    return 0;
+}
