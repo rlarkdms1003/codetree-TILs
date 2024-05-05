b7110c4a30bcce2eef31ed94f4b96664d4eeff28
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-05 21:57:00 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240505/청소당번 정하기/cleaning-numbering.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int cls=0; int cor=0; int rest=0;
+    scanf("%d", &n);
+    for(i=1; i<n; i++) {
+        if(i%12==0) {
+            rest++;
+        }
+        else if(i%3==0) {
+            cor++;
+        }
+        else if(i%2==0) {
+            cls++;
+        }
+    }
+    printf("%d %d %d", cls, cor, rest);
+    return 0;
+}
