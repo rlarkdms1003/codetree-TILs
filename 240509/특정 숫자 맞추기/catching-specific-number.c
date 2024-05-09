7531fb7d5e2347d5f3b25aef678b9f96ee01088f
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-09 22:59:08 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/특정 숫자 맞추기/catching-specific-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    while(1) {
+        scanf("%d", &n);
+        if(n>25) {
+            printf("Lower\n");
+        }
+        else if(n<25) {
+            printf("Higher\n");
+        }
+        else {
+            printf("Good\n");
+            break;
+        }
+    }
+    return 0;
+}
