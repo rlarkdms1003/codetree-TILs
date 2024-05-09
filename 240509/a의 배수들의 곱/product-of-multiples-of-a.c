1c0cd8c5d561a90a75686a6ad8ca0a79850ac731
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-09 13:11:56 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/a의 배수들의 곱/product-of-multiples-of-a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    int prod=1;
+    scanf("%d %d", &a, &b);
+    for(int i=1; i<=b; i++) {
+        if(i%a==0) {
+            prod *= i;
+        }
+    }
+    printf("%d", prod);
+    return 0;
+}
