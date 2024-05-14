3f8b4d2511fe79d4002d417095b3f3cc4c03e39f
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-14 20:19:13 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240514/합성수 판별기/factorization-discriminator.c
+++ 240514/합성수 판별기/factorization-discriminator.c
@@ -4,7 +4,7 @@ int main() {
     int n;
     bool satisfied = false;
     scanf("%d", &n);
-    for(int i=1; i<n; i++) {
+    for(int i=2; i<n; i++) {
         if(n%i==0) {
             satisfied = true;
         }
