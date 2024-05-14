85de2d6271e3149a4e72adc64f20ffbee9c8edf0
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-14 20:45:29 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240514/소수 판별기/prime-number-discriminator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int n;
+    bool satisfied = true;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        if(n%i!=0) {
+            satisfied = true;
+        }
+    }
+    if(satisfied == true) {
+        printf("P");
+    }
+    else {
+        printf("C");
+    }
+
+    return 0;
+}
