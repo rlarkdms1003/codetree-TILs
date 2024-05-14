ea5e28be32ed55ae0d239a127a307392c8e72e46
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-14 20:30:35 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240514/공약수의 유무/presence-or-absence-of-a-common-divisor.c
+++ 240514/공약수의 유무/presence-or-absence-of-a-common-divisor.c
@@ -4,8 +4,8 @@ int main() {
     int a,b;
     bool satisfied = false;
     scanf("%d %d", &a, &b);
-    for(int i=1; i<=b; i++) {
-        if(a%i==0 && b%i==0) {
+    for(int i=a; i<=b; i++) {
+        if(1920%i==0 && 2880%i==0) {
             satisfied = true;
         }
         
