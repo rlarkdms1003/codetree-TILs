098507877b1ca4e9a70ee6dc9e35249d39b68822
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-14 20:26:07 +0900

    Title: 공약수의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240514/공약수의 유무/presence-or-absence-of-a-common-divisor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int a,b;
+    bool satisfied = false;
+    scanf("%d %d", &a, &b);
+    for(int i=1; i<=a; i++) {
+        if(a%i==0 && b%i==0) {
+            satisfied = true;
+        }
+        
+    }
+    if(satisfied== true) {
+        printf("1");
+    }
+    else {
+        printf("0");
+    }
+
+    return 0;
+}
