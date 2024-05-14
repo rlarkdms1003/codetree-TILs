caa164d4ba9be8957f25d29abf998df5e5b6b666
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-05-14 20:48:33 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240514/소수 판별기/prime-number-discriminator.c
+++ 240514/소수 판별기/prime-number-discriminator.c
@@ -2,9 +2,9 @@
 #include <stdbool.h>
 int main() {
     int n;
-    bool satisfied = true;
+    bool satisfied = false;
     scanf("%d", &n);
-    for(int i=1; i<=n; i++) {
+    for(int i=1; i<n; i++) {
         if(n%i!=0) {
             satisfied = true;
         }
