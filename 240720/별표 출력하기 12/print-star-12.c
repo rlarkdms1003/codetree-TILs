6cc3c2b495af9e46e9bc3060a831a1f09f94b324
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-20 16:34:42 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/별표 출력하기 12/print-star-12.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+                if(j%2==0) {
+                    if(i==0) {
+                        printf("* ");
+                    }
+                    else {
+                        printf("  ");
+                    }
+                }
+                else {
+                    if(i <=j) {
+                        printf("* ");
+                    }
+                    else {
+                        printf("  ");
+                    }
+                }
+           
+        }
+        printf("\n");
+    }
+    return 0;
+}
