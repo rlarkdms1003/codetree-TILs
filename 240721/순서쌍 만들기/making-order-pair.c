59a4a1439c5a878b29a8beadf1e51b8e5e989e65
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-21 16:30:17 +0900

    Title: 순서쌍 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/순서쌍 만들기/making-order-pair.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n=0;
+    scanf("%d", &n);
+    for(int i=n; i>0; i--) {
+        for(int j=n; j>0; j--) {
+            printf("(%d,%d) ", i, j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
