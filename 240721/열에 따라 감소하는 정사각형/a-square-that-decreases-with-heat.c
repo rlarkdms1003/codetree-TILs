04d75e70b64af23724888a28c4c2301a9bdab088
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-21 18:44:52 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i=n; i>0; i--) {
+        for(int j=n; j>0; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
