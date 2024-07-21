0247853224112447e711d4735bae55bc8506f91b
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-21 18:49:39 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240721/행과 열/row-column.c
+++ 240721/행과 열/row-column.c
@@ -3,8 +3,8 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    for(int i=1; i<5; i++) {
-        for(int j=1; j<6; j++) {
+    for(int i=1; i<=a; i++) {
+        for(int j=1; j<=b; j++) {
             printf("%d ", i*j);
         }
         printf("\n");
