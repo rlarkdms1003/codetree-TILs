d62a791a17356c7de34fea6719a6fb9d5b3469de
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-27 17:49:51 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/구구단 만들기 2/print-multiplication-table-2.c
+++ 240727/구구단 만들기 2/print-multiplication-table-2.c
@@ -3,16 +3,15 @@
 int main() {
     int a, b;
     scanf("%d%d", &a, &b);
-            for(int j=a; j<10 ; j+=2) {
+            for(int j=2; j<10 ; j+=2) {
              for(int i=b; i>=a; i--) {
-                if(j%2==0) {
             printf("%d * %d = %d", i, j, i*j);
-            if(i!=2) {
+            if(i!=a) {
                 printf(" / ");
             }
                 }
+                printf("\n");
         }
-         printf("\n");
-    }
+    
     return 0;
 }
