b13166f2521013ec40eceda122160bb30c55af8f
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-07-27 16:52:45 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240727/구구단 만들기/print-multiplication-table.c
+++ 240727/구구단 만들기/print-multiplication-table.c
@@ -3,28 +3,32 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    if(b%2==0) {
+    if(b%2==0 && b!=a) {
             for(int j=1; j<10; j++) {
                 for(int i=b; i>1; i-=2) {
                 printf("%d * %d = %d", i, j, i*j);
                 if(i>2) {
                 printf(" / ");
             }
-            }
-            printf("\n");
-           
         }
+        printf("\n");
     }
-    else {
+    }   
+    else if(a!=b && b%2==1) {
             for(int j=1; j<10; j++) {
                 for(int i=b-1; i>1; i-=2) {
                 printf("%d * %d = %d", i, j, i*j);
                 if(i>2) {
-                    printf(" / ");
-                }
+                    printf(" / ");}
             }
             printf("\n");
         }
     }
+    if(a==b && b%2==0) {
+            for(int i=1; i<10; i++) {
+            printf("%d * %d = %d", a, i, a*i);
+            printf("\n");
+            }
+        }
     return 0;
 }
