1265b5454cc276da94d19ec3204eaaa7a49d20af
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-01 16:27:09 +0900

    Title: 배열의 합, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240801/배열의 합/sum-of-array.py
+++ 240801/배열의 합/sum-of-array.py
@@ -1,14 +1,5 @@
 n = 4
-#for _ in range(n):
-    #arr = list(map(int, input().split()))
-    #print(sum(arr))
-arr_2d = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
-n = 4
 for i in range(n):
-    sum_val = 0
-    for j in range(n):
-        sum_val += arr_2d[j][i]
+    arr = list(map(int, input().split()))
+    sum_val = sum(arr)
     print(sum_val)
