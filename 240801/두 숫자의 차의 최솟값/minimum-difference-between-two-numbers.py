48bad904cb78a9cf299db2798fb3056f0370f2bb
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-01 16:23:25 +0900

    Title: 두 숫자의 차의 최솟값, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240801/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+min_difference = float('inf')
+for i in range(n-1):
+    difference = numbers[i+1] - numbers[i]
+    if difference < min_difference:
+        min_difference = difference
+print(min_difference)
