e994138179e575188b4060b67958693c06a96348
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-05 22:58:39 +0900

    Title: 블럭쌓는 명령2, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,8 @@
+N, K = tuple(map(int, input().split()))
+
+arr = [0] * (N+1)
+for _ in range(K):
+    s, e = tuple(map(int, input().split()))
+    for i in range(s, e+1):
+        arr[i] += 1
+print(max(arr))
