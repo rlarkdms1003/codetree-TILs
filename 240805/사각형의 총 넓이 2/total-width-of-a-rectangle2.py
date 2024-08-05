96b6ec3b041aa9306168e94ebf3b9abd3818b6df
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-05 23:18:01 +0900

    Title: 사각형의 총 넓이 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240805/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -8,8 +8,8 @@ offset = 100
 n = int(input())
 for _ in range(n):
     x1, y1, x2, y2 = tuple(map(int, input().split()))
-    for y in range(y1, y2):
-        for x in range(x1, x2):
+    for y in range(y1+offset, y2+offset):
+        for x in range(x1+offset, x2+offset):
             arr[y][x] +=1
 
 ans = 0
