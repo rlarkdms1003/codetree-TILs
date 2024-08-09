1fe302267079e2a0a856a7e3e50fe98004b9e448
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-09 15:23:30 +0900

    Title: 왔다 갔던 구역 2, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 240809/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240809/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,23 +1,46 @@
+OFFSET = 1000
+MAX_R = 2000
+
+# 변수 선언 및 입력
 n = int(input())
-commands = [input().split() for _ in range(n)]
+segments = []
 
-visited = set()
-overlapped = set()
-position = 0
+# 현재 위치
+cur = 0
 
-for x, direction in commands:
-    x = int(x)
-    if direction == "R":
-        for i in range(position + 1, position + x + 1):
-            if i in visited:
-                overlapped.add(i)
-            visited.add(i)
-        position += x
-    elif direction == "L":
-        for i in range(position - 1, position - x - 1, -1):
-            if i in visited:
-                overlapped.add(i)
-            visited.add(i)
-        position -= x
+for _ in range(n):
+	distance, direction = tuple(input().split())
+	distance = int(distance)
+	
+	if direction == 'L':
+		# 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+		section_left = cur - distance
+		section_right = cur
+		cur -= distance
+	else:
+		# 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+		section_left = cur
+		section_right = cur + distance
+		cur += distance
+	
+	segments.append([section_left, section_right])
 
-print(len(overlapped))
+	
+checked = [0] * (MAX_R + 1)
+
+for x1, x2 in segments:
+	# OFFSET을 더해줍니다.
+	x1, x2 = x1 + OFFSET, x2 + OFFSET
+	
+	# 구간을 칠해줍니다.
+	# 구간 단위로 진행하는 문제이므로
+	# x2에 등호가 들어가지 않음에 유의합니다.
+	for i in range(x1, x2):
+		checked[i] += 1
+
+# 2번 이상 지나간 영역의 크기를 구합니다.
+cnt = 0
+for elem in checked:
+	if elem >= 2:
+		cnt += 1
+print(cnt)
