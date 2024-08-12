20719fff2006d9ccd3de1e78eeef022e15da177c
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-12 23:06:07 +0900

    Title: 약수가 세 개인 수, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/약수가 세 개인 수/numbers-with-three-divisors.c
+++ 240812/약수가 세 개인 수/numbers-with-three-divisors.c
@@ -2,11 +2,11 @@
 
 int main() {
     int s, e;
-    int cnt=0;
     int sum =0;
     scanf("%d %d", &s, &e);
     for(int i=s; i<=e; i++) {
-        for(int j=1; j<i; j++) {
+        int cnt=0;
+        for(int j=1; j<=i; j++) {
             if(i%j==0) {
                 cnt++;
             }
