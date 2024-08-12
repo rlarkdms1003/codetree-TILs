e82b40664d96771f0f3b15df71e44489d88c64f0
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-12 16:06:33 +0900

    Title: 3n + 1 수열 m번, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
+++ 240812/3n + 1 수열 m번/3n-plus-1-sequence-m-times.c
@@ -4,20 +4,22 @@ int main() {
     int n;
     int m;
     scanf("%d", &m);
-    int cnt=0;
+
     for(int i=0; i<m; i++) {
         scanf("%d", &n);
-       }
-    
-    while(n !=1) {
+        int cnt=0;
+        while(n !=1) {
         if(n%2==1) {
             n = n*3+1;
             
         }
-        else
-        n = n/2;
+        else 
+        n = n/=2;
+
         cnt++;
     }
-    printf("%d", cnt);
+    printf("%d\n", cnt);
+    }
+    
     return 0;
 }
