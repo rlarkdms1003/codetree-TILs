b4b51d7dbf01815fafb5d90b15ae1e4334056b04
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-08-12 14:13:50 +0900

    Title: 정사각 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/정사각 알파벳 출력하기/print-alpabet-in-square.c
+++ 240812/정사각 알파벳 출력하기/print-alpabet-in-square.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &n);
     for(int i =0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            printf("%c", n+64);
+            printf("%c", 65+j+n*i);
         }
         printf("\n");
     }
