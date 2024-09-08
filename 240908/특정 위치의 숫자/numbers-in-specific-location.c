16ace53d741fb215ba924df9244b079c5d68c227
김가은 <165556231+rlarkdms1003@users.noreply.github.com>
2024-09-08 19:37:24 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/특정 위치의 숫자/numbers-in-specific-location.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    
+    int sum=0;
+    for(int i=0; i<10; i++) {
+        scanf("%d", &arr[i]);
+        if(i ==2 || i==4 || i==9) {
+            sum += arr[i];
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
